0b1a0588d9da71edf875e1a95f9d8aabf1c01619
FreeDaGeek <free.rosas@gmail.com>
2013-10-09 07:11:34 -0400

    Expressions cal

--- Expressions_Worksheet/js/script.js
+++ Expressions_Worksheet/js/script.js
@@ -1,14 +1,31 @@
 /*** Created by Fridelande Rosas on 10/8/13***/
 //Shopping Bill
 
-
-var shoppingBill;
-shoppingBill = ["200", "175", "150", "150", "250"];
-
+//alert ("test")
+var shoppingBill = ["200", "175", "150", "150", "250"];
 console.log(shoppingBill);
 
+var totalBill = [200 + 175 + 150 + 150 + 250];
+console.log(totalBill);
+console.log(totalBill / 5);
+//print AverageBill (Are we to be able to write a sentence in the console.log?
+
+//Dog Years
+
+var sparkyAge;
+
+sparkyAge = 2;
+
+console.log( sparkyAge * 7);
 
 
+//Discount
+
+var taxPrice;
+var nonTaxprice;
+
+taxPrice = 100 - .2;
+    nonTaxPrice = 100 - .2;
 
 
 
