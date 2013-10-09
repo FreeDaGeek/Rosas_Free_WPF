52618cb142b12b3d74cb4a1a653754f6f3a3ce12
FreeDaGeek <free.rosas@gmail.com>
2013-10-09 06:11:44 -0400

    Expressions Assignments WorkSheet

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,1 +1,19 @@
-alert("This is in the js file");
+//alert("Are you connected?");
+
+var famNames =["Carlos", "Free", "Maleeyah"];
+
+famNames[1] = "Mom";
+famNames[0] = "Dad";
+famNames[2] = "Child";
+console.log(famNames[2]);
+
+
+
+var age = 24; //Number Variables
+
+var isStudent = false; //Boolean Variable
+
+var firstName = "Free";
+
+var phrase = 'I don\'t know'; //escape operator
+
---
+++ Expressions_Worksheet/js/script.js
@@ -0,0 +1,18 @@
+/*** Created by Fridelande Rosas on 10/8/13***/
+//Shopping Bill
+
+
+var shoppingBill;
+shoppingBill = ["200", "175", "150", "150", "250"];
+
+console.log(shoppingBill);
+
+
+
+
+
+
+
+
+
+
