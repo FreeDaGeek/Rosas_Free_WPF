2b74e422fd8734fcf03f1aaff37cbee2f914d28e
FreeDaGeek <free.rosas@gmail.com>
2013-10-10 17:16:11 -0400

    Trying to add the totals
    keeps going in as strings

--- Expressions_Personal/js/personal.js
+++ Expressions_Personal/js/personal.js
@@ -2,17 +2,14 @@
 //Fridelande Rosas  Date: 10/10/13
 
 
-var moviesRent;
-moviesRent = ["2", "1", "3", "1"]; //movies rented weekly for a month
+var moviesRent = ["2", "1", "3", "1"]; //movies rented weekly for a month
 console.log(moviesRent);
 
-var totalMovies;
-totalMovies = [moviesRent[0] + moviesRent[1] + moviesRent[2] + moviesRent[3];
+var totalMovies = "moviesRent[0]" + "moviesRent[1]" + "moviesRent[2]" + "moviesRent[3]";
 console.log( totalMovies);
 
 var moviePrice;
-moviePrice = "1";
+moviePrice = 2;
 
-console.log(moviePrice * totalMovies); //total amount spent for 5 wks
-// I changed this to be a reference to the length of the array.
-console.log(totalMovies/moviesRent.length); //average
+console.log(moviePrice * totalMovies); //total amount spent for the month
+console.log(totalMovies/moviesRent.length); //average amount of movies in a month
