904c72e374734980edfbedd89ef59d488fe50fd1
FreeDaGeek <free.rosas@gmail.com>
2013-10-10 18:44:53 -0400

    Complete commit to this folder
    until review

--- Expressions_Wacky/js/wacky.js
+++ Expressions_Wacky/js/wacky.js
@@ -1,11 +1,15 @@
-alert ("test")
+//Wacky
+//Fridelande Rosas 10/10/13
 
 var totalHairlength;
 var years;
+var months;
 
+months = 36
 years = 3; //how long it took
 
 totalHairlength = 15; //hair length in inches
 
 console.log(totalHairlength /years); //average hair growth every year
+console.log(totalHairlength / months) // average hair growth every month
 
