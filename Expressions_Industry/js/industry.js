f949fdf59918d1162e7e7f2da95a51a580bf77d5
FreeDaGeek <free.rosas@gmail.com>
2013-10-10 19:18:09 -0400

    Completed Industry expression

--- Expressions_Industry/js/industry.js
+++ Expressions_Industry/js/industry.js
@@ -1,9 +1,17 @@
-alert( "test")
+//Fridelande Rosas
+//10-10-13
+
 
 var projects;
 var groupMembers;
-var amountOfwork;
+var pagesInproject;
 
-projects = 4;
-groupMembers = 8;
-amountOfwork = groupMembers / projects;
+projects = 4; //amount of projects due
+groupMembers = 5; // the amount of members to work on all projects
+pagesInproject = 5; // amount of things to be done in each project
+
+console.log( projects * pagesInproject); // the total amount of work needs to be done
+console.log(projects * pagesInproject / groupMembers); //how much work each member has to complete
+
+
+
