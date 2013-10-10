976cbedb090a756761ba15624ea26c3cf90c0d49
FreeDaGeek <free.rosas@gmail.com>
2013-10-10 16:56:42 -0400

    Firs Draft, Error messages
    Still working on the math part

--- Expressions_Personal/js/personal.js
+++ Expressions_Personal/js/personal.js
@@ -1,3 +1,16 @@
-/**
- * Created by Frosas32 on 10/10/13.
- */
+//Personal Expressions
+//Fridelande Rosas  Date: 10/10/13
+
+
+var rentMovies;
+rentMovies = ["2", "1" "3", "1"];
+console.log(rentMovies);
+
+var totalMovies;
+totalMovies = [rentMovies[0] + rentMovies[1] + rentMovies[2] + rentMovies[3];
+var moviePrice;
+moviePrice = "1";
+
+console.log(moviePrice * totalMovies); //total amount spent for 5 wks
+// I changed this to be a reference to the length of the array.
+console.log(totalMovies/rentMovies.length); //average
