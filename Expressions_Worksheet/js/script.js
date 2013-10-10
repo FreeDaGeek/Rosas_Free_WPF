2bcd17ac85239d4463daeecd24345ac4488c13c5
josephdalton <jdalton@fullsail.com>
2013-10-10 10:43:34 -0400

    JMD comments

--- Expressions_Worksheet/js/script.js
+++ Expressions_Worksheet/js/script.js
@@ -21,22 +21,38 @@ dogAge = sparkyAge * 7;
 var shoppingBill = ["200", "175", "150", "150", "250"];
 console.log(shoppingBill);
 
-var totalBill = [200 + 175 + 150 + 150 + 250];
+//var totalBill = [200 + 175 + 150 + 150 + 250];
+
+var totalBill = shoppingBill[0]+shoppingBill[1]+shoppingBill[2]+shoppingBill[3]shoppingBill[4]
+
+
 //
 console.log(totalBill); //total amount spent for 5 wks
-console.log(totalBill / 5); //averageBill
+// I changed this to be a reference to the length of the array.
+console.log(totalBill/shoppingBill.length); //averageBill 
+
 
 
 
 
 //Discount
 
-var origPrice = 500; //original price
-var discountPer = .4 * origPrice; //discount percentage (40%)
-var taxPer = 0.06 * discountPer; // Sales Tax
+//var origPrice = 500; //original price
+//var discountPer = .4 * origPrice; //discount percentage (40%)
+//var taxPer = 0.06 * discountPer; // Sales Tax
 
-console.log(origPrice - discountPer + taxPer);
-console.log(origPrice - discountPer);
+//console.log(origPrice - discountPer + taxPer);
+//console.log(origPrice - discountPer);
+
+//JMD Comment
+//Define in termes of what you need.
+var originalPrice = 100;
+var discount = .05;
+var taxRate = .2;
+// Define what you are solving for step by step
+var discountPrice = 'Fill this in'
+var discountWithTax= 'Fill this in'
+
 
 
 
