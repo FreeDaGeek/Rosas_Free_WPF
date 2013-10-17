5fcdc67e7fedc41a69847555c6e76f4e6f3ec748
FreeDaGeek <free.rosas@gmail.com>
2013-10-17 15:50:06 -0400

    Moved the Industry code from the worksheet

--- Conditional_Industry/js/industry.js
+++ Conditional_Industry/js/industry.js
@@ -1,3 +1,15 @@
 //Fridelande Rosas
 //10-15-13
 
+
+var payFull = 100;
+(payFull < 100) ? console.log ("We aren't done yet") :  console.log ("You are good to go");
+
+///////Regular Conditional Below/////
+
+if (payFull < 100){
+    console.log( "Client owes me money");
+}else{
+    console.log("Client has paid in full");
+}
+
