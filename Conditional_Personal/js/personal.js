a69dd8a1e90d5a9fc49346d67fbd092c40695ebb
FreeDaGeek <free.rosas@gmail.com>
2013-10-17 14:56:42 -0400

    Working on Personal .js
    Changed the html file name

--- Conditional_Personal/js/personal.js
+++ Conditional_Personal/js/personal.js
@@ -1,3 +1,14 @@
-//Personal Expressions
-//Fridelande Rosas  Date: 10/15/13
+//Over/Under Eating
+//Should I eat this meal?
+var maxCal= 600;
+var mealCal=800;
 
+if(mealCal >= maxCal){
+    console.log("No thanks, I'll have a salad.");
+}else if( mealCal /2 < 500 ){
+    //If I can save the other have if less then 500 cal
+    console.log("I can eat some now and save for later.");}
+
+else{
+    console.log("This meal works for me!");
+}
