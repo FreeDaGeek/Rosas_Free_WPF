24af504893b046f124e2c00a49dd7732a3e2ae61
FreeDaGeek <free.rosas@gmail.com>
2013-10-17 15:47:14 -0400

    re typed the hanted house instead of "fixing"

--- Conditional_Wacky/js/wacky.js
+++ Conditional_Wacky/js/wacky.js
@@ -7,4 +7,10 @@ var hauntedHouse = 3;
 var hauntedHouse2 = 1;
 
 // if the wait time for the 1st house greater then or equal to 3
-
+if(hauntedHouse >= 3){
+    console.log("Ain't nobody got time for that!");
+    //The second haunted house wait time is less we can go there first
+}else if(hauntedHouse2 < hauntedHouse){
+    //Rather go to the shorter line until the first house line goes down
+    console.log("We can go to the second one first.");
+}
