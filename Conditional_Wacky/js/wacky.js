59799a88ffcc9bc6fcbb2ac2444ace6f480ddb47
FreeDaGeek <free.rosas@gmail.com>
2013-10-17 14:34:30 -0400

    Wacky's First attempted

--- Conditional_Wacky/js/wacky.js
+++ Conditional_Wacky/js/wacky.js
@@ -1,3 +1,17 @@
 //Wacky
 //Fridelande Rosas 10/15/13
 
+//Haunted House
+//Is the line for the haunted house to long to wait?
+var hauntedHouse = 3;
+var hauntedHouse2 = 1;
+
+// if the wait time for the 1st house greater then or equal to 3
+if ( hauntedHouse >= 3);{
+    console.log("Ain't nobody got time for that!");
+    //But if the second haunted house wait time is less we can go there first
+}else if (hauntedHouse2 < hauntedHouse){
+    console.log("This Haunted House will do fine for now!")
+}else{
+    console.log("We will go to the second haunted house after.");
+}
