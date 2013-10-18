0c46c0c7d54d4e7ba5ef170ecbc3ca6395c367e5
FreeDaGeek <free.rosas@gmail.com>
2013-10-17 21:22:24 -0400

    Added a prompt for hauntedHouse; Waky

--- Conditional_Wacky/js/wacky.js
+++ Conditional_Wacky/js/wacky.js
@@ -3,7 +3,7 @@
 
 //Haunted House
 //Is the line for the haunted house to long to wait?
-var hauntedHouse = 3;
+var hauntedHouse = prompt("What is the wait time?");
 var hauntedHouse2 = 1;
 
 // if the wait time for the 1st house greater then or equal to 3
