5f3e909e6ed14e5217107210d5c144abea6b7adc
FreeDaGeek <free.rosas@gmail.com>
2013-10-17 20:56:37 -0400

    Conditional: Industry and Personal update

--- Conditional_Personal/js/personal.js
+++ Conditional_Personal/js/personal.js
@@ -1,14 +1,16 @@
 //Over/Under Eating
 //Should I eat this meal?
 var maxCal= 600;
-var mealCal=800;
+var mealCal= prompt("How many calories is this meal?");
 
+//If mealCal amount greater then of equal to the maxCal
 if(mealCal >= maxCal){
     console.log("No thanks, I'll have a salad.");
+    //if the amount of mealCal if maxed, split the meal into 2 if its less then 500
 }else if( mealCal /2 < 500 ){
-    //If I can save the other have if less then 500 cal
     console.log("I can eat some now and save for later.");}
-
+    //the mealCal was less then the max and not equal to
 else{
     console.log("This meal works for me!");
 }
+
