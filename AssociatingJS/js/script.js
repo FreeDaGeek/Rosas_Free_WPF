ebaca17db779e051e457552ff631549ddf511178
FreeDaGeek <free.rosas@gmail.com>
2013-10-23 05:35:50 -0400

    functionsWS
    First Practice- Circumference

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,19 +1,4 @@
-//alert("Are you connected?");
-
-var famNames =["Carlos", "Free", "Maleeyah"];
-
-famNames[1] = "Mom";
-famNames[0] = "Dad";
-famNames[2] = "Child";
-console.log(famNames[2]);
-
-
-
-var age = 24; //Number Variables
-
-var isStudent = false; //Boolean Variable
-
-var firstName = "Free";
-
-var phrase = 'I don\'t know'; //escape operator
-
+/**
+ * Created by Frosas32 on 10/23/13.
+ */
+alert("Is It Working?")
---
+++ FunctionWorkSheet/js/functionWS.js
@@ -0,0 +1,11 @@
+//Circumference
+
+var radius = 6;
+var pi = 3.14;
+var power = 2;
+
+function circumference(radius, pi, power)
+{
+
+    return circumference(radius * pi * power);
+}
