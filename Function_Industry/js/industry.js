6329ce47d879de74081fd19f4907cb9b03b3e3ba
FreeDaGeek <free.rosas@gmail.com>
2013-10-25 11:14:45 -0400

    Industry/not so industry

--- Function_Industry/js/industry.js
+++ Function_Industry/js/industry.js
@@ -1,3 +1,15 @@
 /**
  * Created by Frosas32 on 10/25/13.
  */
+//I couldn't think of an industry so I used one that Im in now.
+
+
+//Natural Hair Retention
+var names = new Array(); //variable array names
+names[0] = "Maleeyah"; //index 0 string
+names[1] = "Melissa"; //index 1  string
+names[2] = "Sara"; //index 2  string
+
+var hairLength = prompt("How long is your hair?"); //variable hairLength prompt
+var potentailHairGrowth = .25 * 12; //variable Potential Hair Growth
+var hairCuts = prompt("How often do you trim your hair?"); //variable hairCuts prompt
