e75f7ef3d13e76c08555c67f05e0c07ab777ef53
FreeDaGeek <free.rosas@gmail.com>
2013-10-24 22:37:30 -0400

    Started the Wacky/not so wacky

---
+++ FunctionWacky/js/wacky.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Frosas32 on 10/24/13.
+ */
+var massageCost = calculation(2, 10);
+//Calculation for call
+function calculation(massageLength, perhour){
+    return totalcost = callLength * perhour;
+}
+console.log("It will cost you " + massageCost + "$ to get a 2 hour massage");
