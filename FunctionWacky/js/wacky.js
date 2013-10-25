dec55a9b2972385927e1afbfdf5a9817d6ff3804
FreeDaGeek <free.rosas@gmail.com>
2013-10-25 10:03:53 -0400

    Updated Comments with Wacky

--- FunctionWacky/js/wacky.js
+++ FunctionWacky/js/wacky.js
@@ -1,6 +1,6 @@
 //Halloween
 var sweetTreat = prompt("How much halloween candy did you eat for the day?"); //variable sweetTreat
-var  enoughCandy= 10 + "pieces"; //enoughCandy variable
+var  enoughCandy= 15 + "pieces"; //enoughCandy variable
 
 var totalAmount = function Water(){ //variable totalamount function
     var totalMath = enoughCandy - sweetTreat; //variable total math
@@ -9,6 +9,6 @@ var totalAmount = function Water(){ //variable totalamount function
 
 var Candy = totalAmount(); //variable Candy
 
-!(sweetTreat > enoughCandy) ? console.log("No need to over do it, you have enough today.") : console.log("I don't see why you can have a few more");
-// ternary  cost you " + massageCost + "$ to get a 2 hour massage");
+!(sweetTreat > enoughCandy ) ? console.log("No need to over do it, you have enough today.") : console.log("I don't see why you can have a few more");
+// ternary
 
